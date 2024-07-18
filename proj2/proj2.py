62e541fd5c5c2930638a385aa6b8d9ef582ca2a1
16cconway <16cconway@gmail.com>
2024-07-18 13:30:34 -0400

    updates

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -6,14 +6,14 @@ from time import time_ns
 from runtime_metric import RuntimeMetric
 
 
-def get_valid_char(in_str: str) -> str:
+def get_valid_char(in_str: str) -> tuple:
     space_chars = ["\n", "\t", " "]
 
     char = in_str[0]
-    while char not in space_chars:
-        char = in_str[0]
+    while char in space_chars:
         in_str = in_str[1:]
-    return char
+        char = in_str[0]
+    return char, in_str
 
 
 def convert_prefix_to_postfix_recursive(in_str: str) -> tuple:
@@ -28,22 +28,22 @@ def convert_prefix_to_postfix_recursive(in_str: str) -> tuple:
 
     valid_ops = ["+", "-", "*", "/", "$", "^"]
 
-    char = in_str[0]
-
-
+    char, in_str = get_valid_char(in_str)
     in_str = in_str[1:]
 
-    if in_str[0] in valid_ops:
+    temp, in_str = get_valid_char(in_str)
+    if temp in valid_ops:
         char1, in_str = convert_prefix_to_postfix_recursive(in_str)
 
     else:
-        char1 = in_str[0]
+        char1, in_str = get_valid_char(in_str)
         in_str = in_str[1:]
 
-    if in_str[0] in valid_ops:
+    temp, in_str = get_valid_char(in_str)
+    if temp in valid_ops:
         char2, in_str = convert_prefix_to_postfix_recursive(in_str)
     else:
-        char2 = in_str[0]
+        char2, in_str = get_valid_char(in_str)
         in_str = in_str[1:]
 
     post = char1 + char2 + char
@@ -53,8 +53,16 @@ def convert_prefix_to_postfix_recursive(in_str: str) -> tuple:
 with open("../resources/input/RequiredInput.txt") as input_file:
     lines = input_file.readlines()
 
-inp = "++AAAA" #lines[0]
+inp = lines[0]
+
+res, end_str = convert_prefix_to_postfix_recursive(inp)
 print(inp)
-post, end_str = convert_prefix_to_postfix_recursive(inp)
-print(post)
-print(end_str)
+print(res)
+
+
+def helper():
+    for line in lines:
+        if line[len(line)-1] == "\n":
+            write(f"prefix: {line}")
+        else:
+            write(f"prefix: {line} \n")
