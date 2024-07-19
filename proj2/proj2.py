dc2cbb7b21081065726680cee74e7aa1662f9bfd
16cconway <16cconway@gmail.com>
2024-07-18 22:15:49 -0400

    Add some error checking

--- proj2/proj2.py
+++ proj2/proj2.py
@@ -9,12 +9,17 @@ from typing import TextIO
 def get_valid_char(in_str: str) -> tuple:
     space_chars = ["\n", "\t", " "]
 
-    char = in_str[0]
+    if not bool(in_str):
+        return -1, in_str
+    else:
+        char = in_str[0]
+
     while char in space_chars:
         in_str = in_str[1:]
         if not bool(in_str):
-            return 0, in_str
+            return -1, in_str
         char = in_str[0]
+
     return char, in_str
 
 
@@ -34,16 +39,23 @@ def convert_prefix_to_postfix_recursive(in_str: str) -> tuple:
     in_str = in_str[1:]
 
     temp, in_str = get_valid_char(in_str)
-    if temp in valid_ops:
+    if temp == -1:
+        return -1, in_str
+    elif temp in valid_ops:
         char1, in_str = convert_prefix_to_postfix_recursive(in_str)
-
+        if char1 == -1:
+            return -1, in_str
     else:
         char1, in_str = get_valid_char(in_str)
         in_str = in_str[1:]
 
     temp, in_str = get_valid_char(in_str)
-    if temp in valid_ops:
+    if temp == -1:
+        return -1, in_str
+    elif temp in valid_ops:
         char2, in_str = convert_prefix_to_postfix_recursive(in_str)
+        if char2 == -1:
+            return -1, in_str
     else:
         char2, in_str = get_valid_char(in_str)
         in_str = in_str[1:]
@@ -54,12 +66,31 @@ def convert_prefix_to_postfix_recursive(in_str: str) -> tuple:
 
 def starter_func(input_file: TextIO, output_file: TextIO) -> None:
     lines = input_file.readlines()
+    line_idx = 0
     for line in lines:
-        post, in_str = convert_prefix_to_postfix_recursive(line)
+        line_idx += 1
 
+        # Check if line is blank
+        temp = get_valid_char(line)
+        if temp[0] == -1:
+            continue
+
+        post, end_str = convert_prefix_to_postfix_recursive(line)
+
+        # Comment
         if line[len(line)-1] == "\n":
             output_file.write(f"Prefix: {line}")
         else:
             output_file.write(f"Prefix: {line} \n")
 
-        output_file.write(f"Postfix: {post}\n\n")
+        # Comment
+        next_char, end_str = get_valid_char(end_str)
+
+        if post == -1:
+            output_file.write(f"Error input line {line_idx}: Too many "
+                              f"operators \n\n")
+        elif next_char != -1:
+            output_file.write(f"Error input line {line_idx}: Not enough "
+                              "operators \n\n")
+        else:
+            output_file.write(f"Postfix: {post}\n\n")
