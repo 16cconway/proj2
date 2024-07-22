ae662361e8b1f9c7ffbce181024f25e839f7560e
16cconway <16cconway@gmail.com>
2024-07-22 15:16:41 -0400

    edit get_input_size

--- proj2/get_input_size.py
+++ proj2/get_input_size.py
@@ -5,11 +5,19 @@ def get_input_size(lines: list) -> int:
     :param lines: Lines from input file
     :return: count: the total number of characters in the input file
     """
+
+    valid_ops = ["+", "-", "*", "/", "$", "^"]
+
     count = 0
 
     # iterate over all characters within each line of lines and add 1 to count
     for string in lines:
-        for _ in string:
+        for char in string:
             count += 1
+
+            # If read character is invalid, continue to next line to mirror how the conversion function would handle
+            # invalid character inputs
+            if not char.isalpha() or char not in valid_ops:
+                continue
 
     return count
